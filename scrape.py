7792eccd08a7c1e6bd5ce7beb0fe5503388d2a57
Scott McAllister <mr.scott.mcallister@gmail.com>
2016-12-10 17:17:19 -0500

    start writing to CSV

--- scrape.py
+++ scrape.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import lxml as lxml
+import csv
 
 url = 'http://www.basketball-reference.com/contracts/players.html'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'lxml')
 
-rank = []
-player_name = []
-team = []
-season_salary = []
+player_names = []
+teams = []
+season_salaries = []
 contract_type = []
 
 table = soup.find(class_='stats_table')
@@ -22,10 +22,28 @@ def print_range(start_i, end_i):
         name = BeautifulSoup(col[0].string, 'lxml').text
         team = BeautifulSoup(col[1].string, 'lxml').text
         salary = BeautifulSoup(col[2].string, 'lxml').text
+        contract = BeautifulSoup(col[8].string, 'lxml').text
         print "name: "+str(name)+" team: "+str(team)+" salary: "+str(salary)
+        player_names.append(name)
+        teams.append(team)
+        season_salaries.append(salary)
+        contract_type.append(contract)
 
 print_range(start_i=2, end_i=22)
 print_range(start_i=24, end_i=44)
 print_range(start_i=46, end_i=66)
 print_range(start_i=68, end_i=88)
 print_range(start_i=90, end_i=110)
+
+with open('salaries.csv', 'w') as csvfile:
+    fieldnames = ['player_name', 'team', 'salary', 'contract_type']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for index in range(0, 99):
+        writer.writerow({
+            'player_name': player_names[index],
+            'team': teams[index],
+            'salary': season_salaries[index],
+            'contract_type': contract_type[index]
+        })
