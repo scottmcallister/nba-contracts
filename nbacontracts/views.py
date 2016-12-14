7c4ca24dd52cb4270e6c320685eccd7e92d5679a
Scott McAllister <mr.scott.mcallister@gmail.com>
2016-12-13 23:44:22 -0500

    create test API endpoint

--- nbacontracts/views.py
+++ nbacontracts/views.py
@@ -1,7 +1,15 @@
-from flask import render_template
+from flask import render_template, jsonify
 from nbacontracts import app
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/api/test')
+def api_test():
+    result = {
+        "test": "test"
+    }
+    return jsonify(result)
