a2ca147ada357a1a1c5d04a0c9fdee0ea8239c48
Scott McAllister <mr.scott.mcallister@gmail.com>
2016-12-14 00:08:18 -0500

    group json response by player

--- nbacontracts/views.py
+++ nbacontracts/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, jsonify
 from nbacontracts import app
+import pandas as pd
 
 
 @app.route('/')
@@ -7,9 +8,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/api/merged')
+def api_merged():
+    df = pd.read_csv('./nbacontracts/data/merged.csv')
+    response = df.to_json(orient="records")
+    return jsonify(response)
+
+
 @app.route('/api/test')
 def api_test():
-    result = {
+    response = {
         "test": "test"
     }
-    return jsonify(result)
+    return jsonify(data=response)
